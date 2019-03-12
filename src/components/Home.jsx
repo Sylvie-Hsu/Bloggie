a91b5149647d65b30e474c7748ee29429350d1a1
SylvieHsu <sylviehsu@outlook.com>
2019-03-12 15:35:50 +0800

    Add redux

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,28 +1,32 @@
 import React, { Component } from "react";
-import axios from "axios";
+// import axios from "axios";
 import { Link } from "react-router-dom";
+import Pokeball from "../pokeball.png";
+import { connect } from "react-redux";
 
 class Home extends Component {
-  state = {
-    posts: []
-  };
-  componentDidMount() {
-    axios.get("https://jsonplaceholder.typicode.com/posts").then(res => {
-      console.log(res);
-      this.setState({
-        posts: res.data.slice(0, 10)
-      });
-    });
-  }
+  // state = {
+  //   posts: []
+  // };
+  // componentDidMount() {
+  //   axios.get("https://jsonplaceholder.typicode.com/posts").then(res => {
+  //     console.log(res);
+  //     this.setState({
+  //       posts: res.data.slice(0, 10)
+  //     });
+  //   });
+  // }
   render() {
-    const { posts } = this.state;
+    console.log(this.props);
+    const { posts } = this.props;
     const postList = posts.length ? (
       posts.map(post => {
         return (
           <div className="post card" key={post.id}>
+            <img src={Pokeball} alt="A pokeball" />
             <div className="card-content">
               <Link to={"/posts/" + post.id}>
-                <span className="card-title">{post.title}</span>
+                <span className="card-title red-text">{post.title}</span>
               </Link>
               <p>{post.body}</p>
             </div>
@@ -33,7 +37,7 @@ class Home extends Component {
       <div className="center">No posts yet</div>
     );
     return (
-      <div className="container">
+      <div className="container home">
         <h4 className="center">Home</h4>
         {postList}
       </div>
@@ -41,4 +45,10 @@ class Home extends Component {
   }
 }
 
-export default Home;
+const mapStateToProps = state => {
+  return {
+    posts: state.posts
+  };
+};
+
+export default connect(mapStateToProps)(Home);
