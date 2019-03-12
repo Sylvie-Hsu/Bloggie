c196e657378437bfc1148c1cf4ab7ac0f93e3cfb
SylvieHsu <sylviehsu@outlook.com>
2019-03-12 13:52:33 +0800

    Add posts

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,44 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
-const Home = () => {
-  return (
-    <div className="container">
-      <h4 className="center">Home</h4>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit quam ut
-        veritatis at quae dolorem dicta ea harum nesciunt placeat cumque, quia
-        dolore earum odit sunt facere, laudantium vitae? Neque?
-      </p>
-    </div>
-  );
-};
+class Home extends Component {
+  state = {
+    posts: []
+  };
+  componentDidMount() {
+    axios.get("https://jsonplaceholder.typicode.com/posts").then(res => {
+      console.log(res);
+      this.setState({
+        posts: res.data.slice(0, 10)
+      });
+    });
+  }
+  render() {
+    const { posts } = this.state;
+    const postList = posts.length ? (
+      posts.map(post => {
+        return (
+          <div className="post card" key={post.id}>
+            <div className="card-content">
+              <Link to={"/posts/" + post.id}>
+                <span className="card-title">{post.title}</span>
+              </Link>
+              <p>{post.body}</p>
+            </div>
+          </div>
+        );
+      })
+    ) : (
+      <div className="center">No posts yet</div>
+    );
+    return (
+      <div className="container">
+        <h4 className="center">Home</h4>
+        {postList}
+      </div>
+    );
+  }
+}
 
 export default Home;
